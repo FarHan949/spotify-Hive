f9fbfdb86c68347f8053686e9ee19f1d6de5fcc4
FarHan949 <uddinfarhan949@gmail.com>
2024-04-24 14:06:07 +0600

    daynamic card load logic issue fixed

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ async function getSong(folder){
     let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
     let response = await a.text()
     // console.log(response)
-
     let div = document.createElement('div')
     div.innerHTML = response
     let anchor  = div.getElementsByTagName("a")
@@ -57,7 +56,6 @@ async function getSong(folder){
 
        // play audio function 
 const playMusic= (audio, pause=false) =>{
-    // let track = new Audio("/song/" + audio)
     currentSong.src = `/${currentFolder}/` + audio
     if(!pause){
 
@@ -85,6 +83,51 @@ function secondsToMinutes(seconds) {
 }
 
 
+async function displayAlbum(){
+    let a = await fetch(`http://127.0.0.1:5500/song/`)
+    let response = await a.text()
+    let div = document.createElement('div')
+    div.innerHTML = response
+    let anchor  = div.getElementsByTagName("a")
+    let cardContainer = document.querySelector(".cardContainer")
+    let array = Array.from(anchor)
+    
+    for (let index = 0; index < array.length; index++) {
+        const e = array[index];
+
+        if(e.href.includes("/song/")){
+            let folder = e.href.split("/").slice(-1)[0]
+        
+
+        //    get the metadata of the folder 
+        let a = await fetch(`http://127.0.0.1:5500/song/${folder}/info.json`)
+        let response = await a.json()
+        // console.log(response)
+        cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="weekend" class="card">
+        <div class="play">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+                    <path d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 
+                    19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 
+                    5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 
+                    9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z" stroke="black" fill="#000" stroke-width="1.5" 
+                    stroke-linejoin="round" />
+                </svg> 
+        </div>
+            <img src="/song/${folder}/cover.jpg" alt="">
+            <h2>${response.title}</h2>
+            <p>${response.description}</p>
+       </div>`
+    } 
+        
+    }
+    // load the playlist if card was clicked 
+    Array.from(document.getElementsByClassName("card")).forEach(e=>{
+        e.addEventListener('click', async item=>{
+            songs = await getSong(`song/${item.currentTarget.dataset.folder}`)
+            
+        })
+    })
+}
 
 
 
@@ -92,12 +135,15 @@ function secondsToMinutes(seconds) {
 async function main(){
 
     // get the list of the all song 
-     await getSong("song/mood-fresh")
+     await getSong(`song/cps`)
     // console.log(songs)
 
     playMusic(songs[0], true)
 
-   
+    // All albums are shown on the display
+       displayAlbum()
+
+
 // Add event listener to play song  
     play.addEventListener("click", ()=>{
         if(currentSong.paused){
@@ -173,13 +219,7 @@ async function main(){
         // }
     }) 
 
-    // load the playlist if card was clicked 
-    Array.from(document.getElementsByClassName("card")).forEach(e=>{
-        e.addEventListener('click', async item=>{
-            songs = await getSong(`song/${item.currentTarget.dataset.folder}`)
-            
-        })
-    })
+    
     
 }
 
